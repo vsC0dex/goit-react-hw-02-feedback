18387e2683252716705afc893911859d34ba1fd2
vsC0dex <max071010@gmail.com>
2023-01-22 14:48:35 +0200

    add-homework

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,63 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+// import Statistics from './Reviews/Statistics';
+import Statistics from './Rewievs/Statistics';
+// import Controls from './Reviews/Controls';
+import Controls from './Rewievs/Controls';
+// import Section from 'components/Section/';
+// import Section from './Section/Section';
+import Section from './Section/Section';
+// import Notification from './Reviews/Notification';
+import Notification from './Rewievs/Notification';
+
+export default class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  increaseRating = grade => {
+    this.setState(prevState => ({
+      [grade]: prevState[grade] + 1,
+    }));
+  };
+
+  countTotalFeedback = () => {
+    return Object.values(this.state).reduce((acc, item) => acc + item, 0);
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    const goodGrades = this.state.good;
+    const totalGrades = this.countTotalFeedback();
+    return totalGrades === 0 ? 0 : Math.round((goodGrades / totalGrades) * 100);
+  };
+
+  render() {
+    const total = this.countTotalFeedback();
+    const positive = this.countPositiveFeedbackPercentage();
+    const options = Object.keys(this.state);
+    const isAnyGrades = Object.values(this.state).reduce(
+      (acc, item) => acc + item,
+      0
+    );
+
+    return (
+      <>
+        <Section title="Statistics">
+          {!isAnyGrades ? (
+            <Notification message="There is no feedback" />
+          ) : (
+            <Statistics
+              statData={this.state}
+              total={total}
+              positive={positive}
+            />
+          )}
+        </Section>
+        <Section title="Please leave feedback">
+          <Controls onLeaveFeedback={this.increaseRating} options={options} />
+        </Section>
+      </>
+    );
+  }
+}
---
+++ src/components/Rewievs/Statistics/Statistics.jsx
@@ -0,0 +1,39 @@
+import PropTypes from 'prop-types';
+import { StatList, StatItem } from './Statistics.styled';
+
+export default function Statistics({ statData, total, positive }) {
+  return (
+    <>
+      {/* Тепер ця штука маштабуєма)) */}
+      <StatList>
+        {Object.entries(statData).map(item => {
+          const statName = item[0];
+          const statValue = item[1];
+          return (
+            <StatItem key={statName}>
+              <span>{statName}:</span> {statValue}
+            </StatItem>
+          );
+        })}
+      </StatList>
+      <StatList>
+        <StatItem>
+          <span>Total:</span> {total}
+        </StatItem>
+        <StatItem>
+          <span>Positive:</span> {positive}%
+        </StatItem>
+      </StatList>
+    </>
+  );
+}
+
+Statistics.propTypes = {
+  statData: PropTypes.shape({
+    good: PropTypes.number.isRequired,
+    neutral: PropTypes.number.isRequired,
+    bad: PropTypes.number.isRequired,
+  }),
+  total: PropTypes.number.isRequired,
+  positive: PropTypes.number.isRequired,
+};
